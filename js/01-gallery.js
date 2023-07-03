9ba4a32f9b00e2c205e35ff8bc8de1b459e627a8
Vladimir <kartcev2204@gmail.com>
2023-07-03 19:45:49 +0300

    3.07.23

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+const markup = createGalleryItems(galleryItems);
 
-console.log(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", markup);
+function createGalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join("");
+}
+const onClick = (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const source = e.target.dataset.source;
+  const alt = e.target.alt;
+  const instance = basicLightbox.create(
+    `<img src="${source}" alt="${alt}" width='800' height='600'/>`
+  );
+  instance.show();
+
+  const instanceClose = () => {
+    instance.close();
+    document.removeEventListener("keydown", keyDown);
+  };
+
+  const keyDown = (evt) => {
+    if (evt.key === "Escape") {
+      instanceClose();
+    }
+  };
+  document.addEventListener("keydown", keyDown);
+};
+galleryList.addEventListener("click", onClick);
